13e460a08ba7ce6be3d4939d009defd0b9dfcecd
Parker Bronson <parbro3@gmail.com>
2020-01-31 11:31:43 -0700

    Quick images completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,142 @@
 import React from 'react';
 import logo from './logo.svg';
+import michael from './Michael.jpg'
+import kellie from './Kellie.jpg'
+import brinley from './Brinley.jpg'
+import jessica from './Jessica.jpg'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className="App-header">
+
+        <div className="wholeDisplay">
+
+          <div className="mainPerson">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" width="300px" src={michael} />
+              <Card.Body>
+                <Card.Title>Michael Ballstaedt</Card.Title>
+                <Card.Text>
+                  I'm an absolute sweetheart just looking for my honey bun
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+
+
+          <div className="carousel">
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={kellie} />
+                <Card.Body>
+                  <Card.Title>Kellie Bronson</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={brinley} />
+                <Card.Body>
+                  <Card.Title>Brinley Miller</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={jessica} />
+                <Card.Body>
+                  <Card.Title>Jessica Alba</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={kellie} />
+                <Card.Body>
+                  <Card.Title>Kellie Bronson</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={brinley} />
+                <Card.Body>
+                  <Card.Title>Brinley Miller</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={jessica} />
+                <Card.Body>
+                  <Card.Title>Jessica Alba</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={kellie} />
+                <Card.Body>
+                  <Card.Title>Kellie Bronson</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={brinley} />
+                <Card.Body>
+                  <Card.Title>Brinley Miller</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="onePicCarousel">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" width="200px" src={jessica} />
+                <Card.Body>
+                  <Card.Title>Jessica Alba</Card.Title>
+                  <Card.Text>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+
+      </body>
     </div>
   );
 }
