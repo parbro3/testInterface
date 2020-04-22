1cd7b980afb7a99b9c15bbf9a66ecc0fa0cdac5b
Parker Bronson <parbro3@gmail.com>
2020-04-22 13:34:55 -0600

    Scoring working. updating db not connected yet.

--- src/SignUpView.js
+++ src/SignUpView.js
@@ -29,7 +29,14 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(3, 0, 2),
   },
   formItem: {
-    padding: theme.spacing(1,0,2)
+    padding: theme.spacing(1,0,2),
+    alignContent: 'center'
+  },
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    marginBottom: '5%'
   }
 }));
 
@@ -37,102 +44,86 @@ export default function SignUpView() {
   const classes = useStyles();
 
   const [score, setScore] = useState("N/A");
+  const [diabetes, setDiabetes] = useState("N/A");
 
-
+  const [pregnancy, setPState] = useState("");
+  const [glucose, setGState] = useState("");
+  const [bloodPressure, setBPState] = useState("");
+  const [skinThickness, setSState] = useState("");
+  const [insulin, setIState] = useState("");
+  const [bmi, setBState] = useState("");
+  const [diabetesPedigreeFunction, setDState] = useState("");
+  const [age, setAState] = useState("");
 
 
   const calculateScore = () => {
-    console.log('clicked duddee');
 
+    if(pregnancy != "" && glucose != "" && bloodPressure != "" && skinThickness != "" && insulin != "" && bmi != "" && diabetesPedigreeFunction != "" && age != ""){
+      console.log('clicked duddee');
 
-    //http request yo.
-      //this is where i'm going to call the apigateway method
-/*
       const requestBody = {
         "Inputs": {
-            "input1":
-            [
-                {
-                    'Pregnancies': "0",
-                    'Glucose': "0",
-                    'BloodPressure': "0",
-                    'SkinThickness': "0",
-                    'Insulin': "0",
-                    'BMI': "0",
-                    'DiabetesPedigreeFunction': "0",
-                    'Age': "0",
-                    'Outcome': "0",
-                }
+          "input1": {
+            "ColumnNames": [
+              "Pregnancies",
+              "Glucose",
+              "BloodPressure",
+              "SkinThickness",
+              "Insulin",
+              "BMI",
+              "DiabetesPedigreeFunction",
+              "Age",
+              "Outcome"
             ],
+            "Values": [
+              [
+                pregnancy,
+                glucose,
+                bloodPressure,
+                skinThickness,
+                insulin,
+                bmi,
+                diabetesPedigreeFunction,
+                age,
+                "0"
+              ]
+            ]
+          }
         },
         "GlobalParameters": {}
-    }
-    */
-
-    const requestBody = {
-      "Inputs": {
-        "input1": {
-          "ColumnNames": [
-            "Pregnancies",
-            "Glucose",
-            "BloodPressure",
-            "SkinThickness",
-            "Insulin",
-            "BMI",
-            "DiabetesPedigreeFunction",
-            "Age",
-            "Outcome"
-          ],
-          "Values": [
-            [
-              "0",
-              "0",
-              "0",
-              "0",
-              "0",
-              "0",
-              "0",
-              "0",
-              "0"
-            ],
-            [
-              "0",
-              "0",
-              "0",
-              "0",
-              "0",
-              "0",
-              "0",
-              "0",
-              "0"
-            ]
-          ]
-        }
-      },
-      "GlobalParameters": {}
-    }
-
+      }
+  
       const requestOptions = {
-        method: 'POST',
-        headers: { 'Accept':'application/json','Content-Type':'application/json','Authorization': 'Bearer XxNYiPfX0vAj4b9XTdrwSpnXqTMeoB88aO7ymtyk+ieI7Y8HbybqGONxypPQF4haHlO1FiiO83V+H7hlyNpZgQ=='},
-        body: JSON.stringify(requestBody)
-    };
-    fetch('https://ussouthcentral.services.azureml.net/workspaces/4c3dc530a4aa454bb5eb182869203b08/services/4142366cbe68443f8fbbe48e2d64f766/execute?api-version=2.0&format=swagger', requestOptions)
-        .then(async response => {
+          method: 'POST',
+          headers: {'Accept':'application/json','Ocp-Apim-Subscription-Key':'9a728b5f8e694070b8ab956732e64059','Content-Type':'application/json','Authorization': 'Bearer XxNYiPfX0vAj4b9XTdrwSpnXqTMeoB88aO7ymtyk+ieI7Y8HbybqGONxypPQF4haHlO1FiiO83V+H7hlyNpZgQ=='},
+          body: JSON.stringify(requestBody)
+      };
+  
+      fetch('https://callml.azure-api.net/callMLStudio/workspaces/4c3dc530a4aa454bb5eb182869203b08/services/4142366cbe68443f8fbbe48e2d64f766/execute?api-version=2.0&details=true', requestOptions)
+      .then(async response => {
+          const data = await response.json();
+  
+          // check for error response
+          if (!response.ok) {
+              // get error message from body or default to response status
+              const error = (data && data.message) || response.status;
+              return Promise.reject(error);
+          }
+  
+          console.log('here is the supposed data ' + JSON.stringify(data));
+          const values = data.Results.output1.value.Values[0];
+          console.log('values yo : ' + JSON.stringify(values));
+          setScore(values[values.length - 1]);
+          setDiabetes(values[values.length - 2])
+          
+      })
+      .catch(error => {
+          console.error('There was an error!', error);
+      });
+    } else {
+      setDiabetes('Please fill in all the form fields')
+    }
 
-            console.log('yo here is the response: ' + response.json())
-
-
-            // check for error response
-            if (!response.ok) {
-                console.log('response is okay')
-            }
-
-            setScore("Whoa returnedddd!");
-        })
-        .catch(error => {
-            console.log('Email not sent')
-        });
 
 
 
@@ -148,126 +139,145 @@ export default function SignUpView() {
   
   return (
 
-    
+  <div className={classes.root}>
 
-    <Container component="main">
+    <Typography className={classes.title} component="h1" variant="h1">
+      Final Project IS415
+    </Typography>
 
 
-      <Typography component="h1" variant="h1">
-        Final Project IS415
-      </Typography>
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
 
-      <div className={classes.paper}>
+        <div className={classes.formItem}>
+          <Typography className={classes.title} component="h2" variant="h3">
+            Diabetes: {diabetes}
+          </Typography>
+        </div>
 
-        <Grid container>
-            <Grid container xs={6}>
-
-              <div className={classes.formItem}>
-                <Typography component="h1" variant="h5">
-                  Input Info and Calculate Score: {score}
-                </Typography>
-              </div>
-
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-
-                    <div className={classes.formItem}>
-                      <TextField
-                        variant="outlined"
-                        required
-                        id="field1"
-                        label="Field 1"
-                      />
-                    </div>
-
-                    <div className={classes.formItem}>
-                      <TextField
-                        variant="outlined"
-                        required
-                        id="field2"
-                        label="Field 2"
-                      />
-                    </div>
-
-                    <div className={classes.formItem}>
-                      <Button onClick={() => calculateScore()}
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                      >
-                        Calculate
-                      </Button>
-                      
-                    </div>
-                    
-                  </Grid>
-                </Grid>
+        <div className={classes.formItem}>
+          <Typography className={classes.title} component="h2" variant="h3">
+            Score: {score}
+          </Typography>
+        </div>
 
 
 
-            </Grid>
+        <div className={classes.formItem}>
+          <TextField
+            onChange={e => setPState(e.target.value)}
+            variant="outlined"
+            required
+            id="Pregnancies"
+            label="Pregnancies"
+          />
+        </div>
+
+        <div className={classes.formItem}>
+          <TextField
+            onChange={e => setGState(e.target.value)}
+            variant="outlined"
+            required
+            id="Glucose"
+            label="Glucose"
+          />
+        </div>
+
+        <div className={classes.formItem}>
+          <TextField
+            onChange={e => setBPState(e.target.value)}
+            variant="outlined"
+            required
+            id="BloodPressure"
+            label="BloodPressure"
+          />
+        </div>
+        
+        <div className={classes.formItem}>
+          <TextField
+            onChange={e => setSState(e.target.value)}
+            variant="outlined"
+            required
+            id="SkinThickness"
+            label="SkinThickness"
+          />
+        </div>
+        
+        <div className={classes.formItem}>
+          <TextField
+            onChange={e => setIState(e.target.value)}
+            variant="outlined"
+            required
+            id="Insulin"
+            label="Insulin"
+          />
+        </div>
+        
+        <div className={classes.formItem}>
+          <TextField
+            onChange={e => setBState(e.target.value )}
+            variant="outlined"
+            required
+            id="BMI"
+            label="BMI"
+          />
+        </div>
+
+        <div className={classes.formItem}>
+          <TextField
+            onChange={e => setDState(e.target.value)}
+            variant="outlined"
+            required
+            id="DiabetesPedigreeFunction"
+            label="DiabetesPedigreeFunction"
+          />
+        </div>
+
+        <div className={classes.formItem}>
+          <TextField
+            onChange={e => setAState(e.target.value)}
+            variant="outlined"
+            required
+            id="Age"
+            label="Age"
+          />
+        </div>
+                
+
+
+        <div className={classes.formItem}>
+          <Button onClick={() => calculateScore()}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Calculate
+          </Button>
+
+          <Button onClick={() => addToDB()}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Add to DB
+          </Button>
+              
+        </div>
+      </Grid>
+    </Grid>
+  </div>
 
 
 
-
-
-            <Grid container xs={6}>
-
-              <div className={classes.formItem}>
-                <Typography component="h1" variant="h5">
-                  Add extra data to DB
-                </Typography>
-              </div>
-
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-
-                  <div className={classes.formItem}>
-                      <TextField
-                        variant="outlined"
-                        required
-                        id="field1"
-                        label="Field 1"
-                      />
-                    </div>
-
-                    <div className={classes.formItem}>
-                      <TextField
-                        variant="outlined"
-                        required
-                        id="field2"
-                        label="Field 2"
-                      />
-                    </div>
-                    
-                  </Grid>
-                </Grid>
-
-                <div className={classes.formItem}>
-                      <Button onClick={() => addToDB()}
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                      >
-                        Add to DB
-                      </Button>
-                      
-                </div>
-
-
-
-            </Grid>
-
-
-        </Grid>
-
-
-
-      </div>
-    </Container>
   );
+
+
 }
+
+
+
+
